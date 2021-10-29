5e660f78ff2f1168359410eccd78406d57d33340
David Sloan <delus.dlsloan@gmail.com>
2021-10-29 01:00:57 -0600

    ARV not maintained... may re-add later

--- coop.cpp
+++ coop.cpp
@@ -1,7 +1,5 @@
 #if defined(ARDUINO_ARCH_SAMD)
 #include "coop_samd.cpp.h"
-#elif defined(ARDUINO_ARCH_AVR)
-#include "coop_arv.cpp.h"
 #else
 static_assert(false, "Unsupported target archatecture");
 #endif
